613a4702274ddc217a8780049a194b848278adaf
yousif <yousifsayer@gmail.com>
2024-04-15 19:34:50 +0300

    add fastapi folder

---
+++ frontend/main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import streamlit as st
+
+from frontend.pages.admin import create_form
+from frontend.pages.index import create_tabel
+from st_pages import Page,show_pages
+
+if 'database' not in st.session_state:
+    st.session_state['database'] = {}
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    create_tabel()
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
