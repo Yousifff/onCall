38472b402da789a7b1086722572511861099c89d
Yousifff <yousifsayer@gmail.com>
2024-04-18 08:29:33 +0300

    .

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from models import User
+from models import User, Weekly
 from database import SessionDep, create_db_and_tables
-
+from datetime import date
+from typing import Optional
 
 
 @asynccontextmanager
@@ -21,9 +22,17 @@ def create_user(user: User, session: SessionDep):
     session.add(user)
     session.commit()
     session.refresh(user)
-    
-
     return user
+
+
+@app.post("/weekly")
+def create_weekly(start_date: date, end_date: date, session: SessionDep):
+    weekly = Weekly(start_date=start_date, end_date=end_date)
+    session.add(weekly)
+    session.commit()
+    session.refresh(weekly)
+    return weekly
+
 
 @app.get("/")
 def get_user(session: SessionDep):
--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,16 @@
-from sqlmodel import SQLModel, Field
-
+from sqlmodel import SQLModel, Field , Relationship
+from datetime import date
+from typing import List
 
 class User(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     name: str
-    
+    weekly: List["Weekly"] = Relationship(back_populates="user")
+
+class Weekly(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    start_date: date  # Corrected to use Date data type
+    end_date: date    # Corrected to use Date data type
+    user_id: int = Field(foreign_key="user.id")
+    user: User = Relationship(back_populates="weekly")
     
